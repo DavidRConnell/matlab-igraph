dfd78b7ef2dd29f21d1a1402806b900326ec2aa8
David R Connell <davidconnell12@gmail.com>
2023-08-18 13:07:35 -0500

    Fix up some help blocks

--- toolbox/+igraph/generate.m
+++ toolbox/+igraph/generate.m
@@ -10,9 +10,9 @@ function adj = generate(method, adjOptions, methodOptions)
 %       'petersen'.
 %
 %   ADJ = GENERATE(..., 'PARAM1', VAL1, 'PARAM2', VAL2, ...) in addition to
-%       method specific parameters, there are parameters for specifying the
-%       how the resulting ADJ is represented common to all methods. These
-%       follow the same conventions as the rest of matlab-igraph.
+%       method specific parameters, there are parameters for specifying how the
+%       resulting ADJ is represented common to all methods. These follow the
+%       same conventions as the rest of matlab-igraph.
 %
 %        Name           Description
 %       --------------------------------------------------------------------
