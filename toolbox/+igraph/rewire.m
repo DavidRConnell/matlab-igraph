073c2a59752b3a04ff5ed9bb821366987aac0e98
David R Connell <davidconnell12@gmail.com>
2023-10-31 22:44:18 -0500

    Add graph rewiring methods

--- toolbox/+igraph/rewire.m
+++ toolbox/+igraph/rewire.m
@@ -1,21 +1,73 @@
-function adj = rewire(adj, method, adjOptions, methodOptions)
-% USE: igraph_rewire_edges, igraph_rewire_directed_edges, igraph_connect_neighborhood
-% (https://igraph.org/c/doc/igraph-Operators.html#miscellaneous-operators),
+function adj = rewire(adj, adjOptions, methodOptions)
+%REWIRE alter an adjacency matrix by rewiring its edges
+%   ADJ = REWIRE(ADJ) rewire the ADJ while preserving it's degree.
+%   ADJ = REWIRE(ADJ, "PROBABILITY", P) rewire an edge with probability P.
+%   ADJ = REWIRE(ADJ, "NREWIRES", N) rewire N edges.
+%   ADJ = REWIRE(ADJ, "PRESERVEDEGREE", TF) If true (default) preserve the
+%         degree (in and out) of each node. Requires mode to be 'all'. If
+%         false, when rewiring, edges are taken from one node pair and move to
+%         another at random.
+%   ADJ = REWIRE(ADJ, "MODE", MODE) Whether to rewire both ends ('all',
+%         default) only 'in' node or only 'out' node. Must be 'all' if
+%         preserving degrees.
+%   ADJ = REWIRE(ADJ, "LOOPS", TF) Whether to allow modifying existing loops
+%         and creating new loops.
 %
-% Look closer at rewire_edges and rewired_directed_edges, may want to use a
-% optional like in, out, both to combine them into a single function.
+%   ADJ = GAME(..., 'PARAM1', VAL1, 'PARAM2', VAL2, ...) provide additional
+%       output name-value pairs. The output defaults to the input's values.
+%       If the input is a sparse logical the output will be a sparse logical.
+%
+%        Name           Description
+%       --------------------------------------------------------------------
+%        'makeSparse'   Whether to return a sparse or full matrix.
+%        'dtype'        The data type to use, either 'double' or 'logical'.
+%        'isdirected'   Whether the resulting ADJ should be directed or not.
+%
+%   See also: igraph.generate, igraph.randgame, igraph.famous,
+%             igraph.correlatedPair.
 
     arguments
         adj {mustBeAdj};
-        method {mustBeMemberi(method, ...
-                              {'probability', 'correlated', 'trial', ...
-                               'neighborhood'})}
         adjOptions.makeSparse (1, 1) logical = issparse(adj);
-        adjOptions.dtype mustBeMemberi(adjOptions.dtype, {'logical', 'double'});
+        adjOptions.dtype (1, :) char ...
+            {mustBeMemberi(adjOptions.dtype, {'logical', 'double'})};
+        adjOptions.isdirected (1, 1) logical = igraph.isdirected(adj);
+        methodOptions.probability (1, 1) ...
+            {mustBeInRange(methodOptions.probability, 0, 1)} = 0;
+        methodOptions.nRewires (1, 1) {mustBeNonnegative, mustBeInteger} = 0;
+        methodOptions.preserveDegree (1, 1) logical = true;
+        methodOptions.mode (1, :) char ...
+            {mustBeMemberi(methodOptions.mode, {'in', 'out', 'all'})} = 'all';
+        methodOptions.loops (1, 1) logical = false;
     end
 
-    method = lower(method);
-    adjOptions.dtype = lower(adjOptions.dtype);
+    if methodOptions.preserveDegree && ~strcmp(methodOptions.mode, 'all')
+        error("Igraph:overConstrained", "Cannot preserve degree without " + ...
+              "modifying both ends of edges.");
+    end
+
+    if methodOptions.probability > 0 && methodOptions.nRewires > 0
+        error("Igraph:overConstrained", "Cannot set both the " + ...
+              "probability of a rewire and the number of rewires.");
+    end
+
+    if methodOptions.probability == 0 && methodOptions.nRewires == 0
+        methodOptions.probability = 0.75;
+    end
+
+    if methodOptions.nRewires > nnz(adj)
+        error("Igraph:tooManyRewires", "Cannot have more rewires than " + ...
+              "edges.\nGraph only has %d edges requested %d rewires.", ...
+              nnz(adj), methodOptions.nRewires);
+    end
+
+    if methodOptions.preserveDegree && methodOptions.nRewires == 0
+        methodOptions.nRewires = ceil(methodOptions.probability * nnz(adj));
+    end
+
+    if ~methodOptions.preserveDegree && methodOptions.probability == 0
+        methodOptions.probability = methodOptions.nRewires / nnz(adj);
+    end
 
     if ~isoptionset(adjOptions, 'dtype')
         if islogical(adj)
@@ -24,8 +76,12 @@ function adj = rewire(adj, method, adjOptions, methodOptions)
             adjOptions.dtype = 'double';
         end
     end
+    adjOptions.dtype = lower(adjOptions.dtype);
 
-    switch method
-        case 'probability'
-    end
+    adj = mexIgraphRewire(adj, adjOptions.makeSparse, adjOptions.dtype, ...
+                          adjOptions.isdirected, ...
+                          methodOptions.preserveDegree, ...
+                          methodOptions.probability, ...
+                          methodOptions.nRewires, ...
+                          methodOptions.mode, methodOptions.loops);
 end
