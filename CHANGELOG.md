0189807132aef05baf347b7a245c1e7db5302dfe
David R Connell <davidconnell12@gmail.com>
2024-02-05 18:52:09 -0600

    Fix arch name used in post install script
    And
    - Find toolbox based of location of +igraph in it's installed locally as a
    different name.
    - Delete self at end since there's no need for the function after it has been
    run.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Fixed
+
+- Post install script not dependent on what the toolbox gets named locally.
+- Post install script gets name of arch that matches mexext function.
+
+### Changed
+
+- Post install function deletes itself after being called.
+
 ## [0.1.5] 2024-02-05
 
 ### Added
--- toolbox/+igraph/postinstall.m
+++ toolbox/+igraph/postinstall.m
@@ -7,7 +7,7 @@ function postinstall()
 %   unneeded files can be cleared with this command.
 
     toolboxDir = findToolbox();
-    currentSystem = lower(computer());
+    currentSystem = computer('arch');
     extList = mexext('all');
     compiledFilePaths = {'+igraph/private'};
 
@@ -19,19 +19,25 @@ function postinstall()
             end
         end
 
-        if ~(startsWith(currentSystem, 'win'))
+        if ~startsWith(currentSystem, 'win')
             delete(fullfile(path{1}, "*.dll"));
         end
 
-        if ~(startsWith(currentSystem, 'gln'))
+        if exist(fullfile(path{1}, 'lib'), 'dir') && ...
+             ~startsWith(currentSystem, 'gln')
             rmdir(fullfile(path{1}, "lib"), "s");
         end
     end
+
+    self = mfilename('fullpath') + ".m";
+    delete(self);
 end
 
 function d = findToolbox()
     fileParts = strsplit(mfilename('fullpath'), filesep);
     rng = 1:length(fileParts);
-    toolboxIdx = rng(strcmp(fileParts, "toolbox"));
+    % Find the dir above +igraph in case the toolbox gets installed under a
+    % new name.
+    toolboxIdx = rng(strcmp(fileParts, "+igraph")) - 1;
     d = strjoin(fileParts(1:toolboxIdx), filesep);
 end
