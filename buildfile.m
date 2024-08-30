f4c8027fa8ca47a5aa21dd07541c6195427cbc17
David R Connell <david32@dcon.addy.io>
2024-08-30 14:28:07 -0500

    Move all doc generation into buildtool
    - All generated files are ignored by git.
    - All file generation is done by the buildtool to provide a single command to
    fully generate the docs.
    - Fix issue reproducing values by clearing mex state between livescript export.

--- buildfile.m
+++ buildfile.m
@@ -3,29 +3,10 @@ function plan = buildfile
     plan.DefaultTasks = "test";
 
     plan("test").Dependencies = ["testMxIgraph" "testToolbox"];
+    plan("makeDocs").Dependencies = ["copyDocFiles" "exportLivescripts"];
 end
 
 function testTask(~)
-end
-
-function makeDocsTask(~)
-    docDir = "docs/content";
-    if ~exist(fullfile(docDir, "start"), "dir")
-        mkdir(fullfile(docDir, "start"));
-    end
-
-    export("toolbox/GettingStarted.mlx", fullfile(docDir, "start", "_index.md"));
-
-    examples = dir("toolbox/examples/*.mlx");
-    dest = fullfile(docDir, "examples");
-    if ~isempty(examples) && ~exist(dest, 'dir')
-        mkdir(dest);
-    end
-
-    for i = 1:length(examples)
-        export(fullfile(examples(i).folder, examples(i).name), ...
-               replace(fullfile(dest, examples(i).name), ".mlx", ".md"));
-    end
 end
 
 function testMxIgraphTask(~)
@@ -38,4 +19,60 @@ function testToolboxTask(~)
     oldPath = addpath("toolbox");
     runtests("tests/toolbox");
     path(oldPath);
+end
+
+function makeDocsTask(~)
+end
+
+function copyDocFilesTask(~)
+    docDir = "docs/content";
+
+    if ~exist(fullfile(docDir, "changelog"))
+        mkdir(fullfile(docDir, "changelog"));
+    end
+
+    copyfile("CHANGELOG.md", fullfile(docDir, "changelog", "_index.md"));
+    copyfile("CONTRIBUTING.org", fullfile(docDir, "docs", "contributing.org"));
+end
+
+function exportLivescriptsTask(~)
+    oldPath = addpath("toolbox");
+
+    dest = "docs/content/start";
+    if ~exist(dest, "dir")
+        mkdir(dest);
+    end
+
+    export("toolbox/GettingStarted.mlx", fullfile(dest, "_index.md"));
+    title2frontmatter(fullfile(dest, "_index.md"));
+
+    examples = dir("toolbox/examples/*.mlx");
+    for i = 1:length(examples)
+        clear mex
+        outfile = replace(fullfile(dest, examples(i).name), ".mlx", ".md");
+        export(fullfile(examples(i).folder, examples(i).name), outfile);
+        title2frontmatter(outfile);
+    end
+
+    path(oldPath);
+end
+
+function title2frontmatter(fname)
+% Convert the title produced by export to Hugo legible frontmatter.
+
+    content = strip(fileread(fname));
+    span_open = "# <span style=""color:rgb\(213,80,0\)"">";
+    span_close = "</span>";
+
+    title = regexp(content, span_open + "(.*)" + span_close, 'once', 'tokens');
+    title = title{1};
+
+    span_open = replace(span_open, "\", "");
+
+    % Without sprintf, the newline characters are left as the literal '\n'.
+    content = replace(content, ...
+                      span_open + title + span_close, ...
+                      sprintf("---\ntitle: " + title + "\ncascade:\n  type: docs\n---\n"));
+
+    writelines(content, fname);
 end
